69cb5394b424f91a811f29423254928cef1dd7cd
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-08-29 17:00:51 -0500

    Last changes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Todo } from './Todo';
-import { TodoList} from './TodoList';
+import { TodoList } from './TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   render() {
-    const todos = [{text:"Learn React", priority:5, dueDate: new Date() },
-          {text:"Learn about APIs", priority:4, dueDate: new Date(2018,8,30) },
-          {text:"write TODO App", priority:3, dueDate: new Date(2018,9,30) }];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">TODO React App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <TodoList list={todos}></TodoList>
+        <h3>TODO</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+        <TodoList list={this.state.items}></TodoList>
       </div>
     );
+  }
+
+  handleChange(e) {
+    console.log(e);
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      priority: 5,
+      dueDate: Date.now(),
+
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,10 +3,6 @@ import {Todo} from './Todo'
 
 export class TodoList extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }   
-
     render() {
         const todos = this.props.list.map((todo) =>
         <Todo text={todo.text} priority={todo.priority} dueDate={todo.dueDate}/>
